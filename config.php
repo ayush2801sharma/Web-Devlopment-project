0ea0f78094a70ea3d2bb9717822000d36b1d9187
ayush2801sharma <ayush.sharma2@spit.ac.in>
2022-03-21 11:09:08 +0530

    change

--- config.php
+++ config.php
@@ -3,15 +3,15 @@
 <?php
 
 
-define('DB_SERVER','localhost');
+define('DB_SERVER','127.0.0.1');
 define('DB_USERNAME','root');
-define('DB_PASSWORD','');
+define('DB_PASSWORD','tnt123');
 define('DB_NAME','login');
 
 $conn = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_NAME);
 
 if($conn == false){
-    dir('Error : Cannot connect');
+    die('Error : Cannot connect');
 
 }
 
